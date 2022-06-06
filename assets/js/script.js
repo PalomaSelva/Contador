e203363bb587ae568877d553541633b0ec49378c
PalomaSelva <palomaselva1@gmail.com>
2022-06-06 14:55:26 -0300

    Contador

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ function decrement() {
     currentNumber= currentNumber-1
     currentNumberWrapper.innerHTML=currentNumber
 }
-
 function increment() {
     currentNumber= currentNumber+1
     currentNumberWrapper.innerHTML=currentNumber
